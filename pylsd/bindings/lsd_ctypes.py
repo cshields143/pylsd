873ab00849393be798776a7ef4c16ec89940ebc8
Chris Shields <christopher.shields143@gmail.com>
2021-04-04 16:33:48 -0700

    update lsdlib loading in bindings.ctypes

--- pylsd/bindings/lsd_ctypes.py
+++ pylsd/bindings/lsd_ctypes.py
@@ -6,6 +6,6 @@ import numpy as np
 from pathlib import Path
 
 DIR = os.path.abspath(os.path.dirname(__file__))
-PATH = Path(DIR)/'lib'/'linux'/'liblsd.so'
+PATH = Path(DIR)/'lib'/'liblsd.so'
 lsdlib = ctypes.cdll[PATH]
 
