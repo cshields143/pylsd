e6fbfee01ea9a276dc9f0fac959e3252a4675930
Chris Shields <christopher.shields143@gmail.com>
2021-04-04 16:26:06 -0700

    simplify lsdlib loading

--- pylsd/bindings/lsd_ctypes.py
+++ pylsd/bindings/lsd_ctypes.py
@@ -3,40 +3,15 @@ import os
 import sys
 import random
 import numpy as np
-
+from pathlib import Path
 
 def load_lsd_library():
-
     root_dir = os.path.abspath(os.path.dirname(__file__))
-
-    libnames = ['linux/liblsd.so']
+    libname = 'linux/liblsd.so'
     libdir = 'lib'
-
-    while root_dir != None:
-        for libname in libnames:
-            try:
-                lsdlib = ctypes.cdll[os.path.join(root_dir, libdir, libname)]
-                return lsdlib
-            except Exception as e:
-                pass
-        tmp = os.path.dirname(root_dir)
-        if tmp == root_dir:
-            root_dir = None
-        else:
-            root_dir = tmp
-
-    # if we didn't find the library so far, try loading without
-    # a full path as a last resort
-    for libname in libnames:
-        try:
-            # print "Trying",libname
-            lsdlib = ctypes.cdll[libname]
-            return lsdlib
-        except:
-            pass
-
-    return None
-
+    lsdlib = ctypes.cdll[Path(root_dir)/libdir/libname]
+    return lsdlib
+    
 lsdlib = load_lsd_library()
 if lsdlib == None:
     raise ImportError('Cannot load dynamic library. Did you compile LSD?')
